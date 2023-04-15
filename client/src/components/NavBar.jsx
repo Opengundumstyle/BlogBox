4eaa2e8bc41b2538f27ac7bc28eeee779d6db92b
Zhile Lin <zhlin04@gmail.com>
2023-04-14 17:31:44 -0700

    navbar style

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>
-      nav
+    <div className='navbar'>
+      <div className="container">
+         <div className="logo">Logo</div>
+         <div className="links">Links</div>
+      </div>
     </div>
   )
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Register = () => {
   return (
-    <div>
-      resgister
+    <div className='auth'>
+      <h1>Register</h1>
+      <form>
+         <input required type="text" placeholder='username'/>
+         <input required type="email" placeholder='email'/>
+         <input required type="password" placeholder='password'/>
+         <button>Sign up</button>
+         <p>This is an error!</p>
+         <span>Have an account? <Link to="/login">Login</Link></span> 
+      </form>
     </div>
   )
 }
